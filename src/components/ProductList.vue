4fee12d30c5175a8f52490658f7ab562bacc8af0
Jeremy Joshua R. Jaurigue <jauriguejeremyjoshua02@gmail.com>
2024-03-21 22:32:55 +0800

    Fixed tab and spaces bug
    Fixed tab and spaces bug

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,48 +1,48 @@
 <template class="mt-5">
-  <div>
-    <!--Table of Products-->
-    <h2>List of Available Products</h2>
-    <table class="table ">
-      <thead class="table-primary">
-				<tr>
-					<th>Product Name</th>
-					<th>Product Price</th>
-					<th>Action</th>
-				</tr>
-			</thead>
-			<tbody class="table-secondary">
-				<!--Loop for showing the list of products -->
-				<tr v-for="(product, index) in productList" :key="index">
-					<td>{{product.name }}</td>
-					<td>Php {{ product.price.toFixed(2) }}</td>
-					<td><button type="button" class="btn-success" @click="addToCart(product)">Add To Cart</button></td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
+    <div>
+        <!--Table of Products-->
+        <h2>List of Available Products</h2>
+        <table class="table ">
+            <thead class="table-primary">
+                <tr>
+                    <th>Product Name</th>
+                    <th>Product Price</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody class="table-secondary">
+                <!--Loop for showing the list of products -->
+                <tr v-for="(product, index) in productList" :key="index">
+                    <td>{{product.name }}</td>
+                    <td>Php {{ product.price.toFixed(2) }}</td>
+                    <td><button type="button" class="btn-success" @click="addToCart(product)">Add To Cart</button></td>
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
   export default {
     data() {
-    	return {
-				// product list
-				productList: [
-					{name: 'Frozen French Fries (1kg)', price: 116 },
-					{name: 'Frozen Pork Siomai (500 grams)', price: 85},
-					{name: 'Frozen Cheesy Chicken Tenders (500 grams)', price: 90},
-					{name: 'Ice Cream (Vanilla Flavor) 1L', price: 159},
-					{name: 'Frozen Burger Patties (250 grams)', price: 55},
-					{name: 'Frozen Hotdog (1 kilograms)', price: 148},
-					{name: 'Calamares (500 grams)', price: 109},
-					{name: 'Squid Balls (250 grams)', price: 45},
-					{name: 'Fish Balls (1kg)', price: 85},
-					{name: 'Kikiam (500 grams)', price: 70}
-				],
-      }
+        return {
+            // product list
+            productList: [
+                {name: 'Frozen French Fries (1kg)', price: 116 },
+                {name: 'Frozen Pork Siomai (500 grams)', price: 85},
+                {name: 'Frozen Cheesy Chicken Tenders (500 grams)', price: 90},
+                {name: 'Ice Cream (Vanilla Flavor) 1L', price: 159},
+                {name: 'Frozen Burger Patties (250 grams)', price: 55},
+                {name: 'Frozen Hotdog (1 kilograms)', price: 148},
+                {name: 'Calamares (500 grams)', price: 109},
+                {name: 'Squid Balls (250 grams)', price: 45},
+                {name: 'Fish Balls (1kg)', price: 85},
+                {name: 'Kikiam (500 grams)', price: 70}
+            ],
+        }
     },
     methods: {
-			// function that adds cart from the product list
       addToCart(product) {
         // emitted event into the App.vue
         this.$emit('add-to-cart', { product, quantity: 1 });
